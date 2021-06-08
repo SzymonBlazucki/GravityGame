3cce294dde61eb96d3159ad7cc2e96d667ed7460
lihmds <lithiumhmds@protonmail.com>
2021-06-08 17:42:22 +0200

    refactor update()

--- player.py
+++ player.py
@@ -21,22 +21,30 @@ class Player(pygame.sprite.Sprite):
         self.vel = vec(vel[0], vel[1])
         self.omega = 0
 
-    def update(self):
-        tempAccel = vec(0, 0)
+    def get_accel(self):
+        ownFrameAccel = vec(0, 0)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            tempAccel.x -= sideThrusterA
-            self.omega -= sideThrusterAlpha * timeStep
+            ownFrameAccel.x -= sideThrusterA
         if keys[pygame.K_RIGHT]:
-            tempAccel.x += sideThrusterA
-            self.omega += sideThrusterAlpha * timeStep
+            ownFrameAccel.x += sideThrusterA
         if keys[pygame.K_UP]:
-            tempAccel.y -= mainThrusterA
-        # find the angle between y axis upwards and velocity vector
-        rotate = self.vel.angle_to(vec(0, -1))
-        #to accelerate in appropriate direction
-        accel = tempAccel.rotate(-rotate)
-        dVel = accel * timeStep
+            ownFrameAccel.y -= mainThrusterA
+        angleToVertical = self.vel.angle_to(vec(0, -1))
+        return ownFrameAccel.rotate(-angleToVertical)
+
+    def get_alpha(self):
+      alpha = 0
+      keys = pygame.key.get_pressed()
+      if keys[pygame.K_LEFT]:
+          alpha -= sideThrusterAlpha
+      if keys[pygame.K_RIGHT]:
+          alpha += sideThrusterAlpha
+      return alpha
+
+    def update(self):
+        self.omega += self.get_alpha() * timeStep
+        dVel = self.get_accel() * timeStep
         angleDiff = self.vel.angle_to(self.vel + dVel) #+ self.omega * timeStep
         self.vel += dVel
         self.angle -= angleDiff
